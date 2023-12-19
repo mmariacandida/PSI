43f79955f13f351101da71bba26868e557f76f32
20201011110016 <20201011110016@CNAT407146>
2023-12-19 11:16:36 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Controllers/CursoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CursoController : Controller
+    {
+        // GET: Curso
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("Asp_Net_MVC_CS")
+        {
+            Database.SetInitializer<Context>(new DropCreateDatabaseIfModelChanges<Context>());
+        }
+        public DbSet<AreaConhecimento> AreaConhecimento { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+    }
+}
